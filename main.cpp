05a27593e33fe24aa20f9a36c356fb5ee54ec324
yagodnitsyna <42728548+yagodnitsyna@users.noreply.github.com>
2018-08-27 16:14:01 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class FourVector
+{
+public:
+	// add constructor here
+
+	// add methods here
+private:
+	// add variables here
+
+};
+
+
+int main ()
+{
+	FourVector v1, v2, v3;
+
+	v3 = v1 + v2;
+
+	cout << "The sum of vectors is " << v3 << endl;
+	return 0;
+}
